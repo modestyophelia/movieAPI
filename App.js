4ebf62077908c9c8d3d15b5c0bbfbc1a55de45ef
Modesty Ophelia Aguilar <ophelia.m.aguilar@gmail.com>
2021-12-10 14:47:29 +0100

    first push
    put everything into a brand new repository so that the app could have a new more fitting name...

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://reactnative.dev/movies.json")
+      .then((response) => response.json())
+      .then((result) => setData(result.movies))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item, index }) => (
+            <Text style={styles.movies}>
+              {item.id}, {item.title}, {item.releaseYear},
+            </Text>
+          )}
+        />
+      )}
     </View>
   );
 }
@@ -14,8 +40,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    alignItems: "center",
+  },
+
+  movies: {
+    fontSize: 30,
+    margin: 10,
+    padding: 10,
+    color: "white",
+    backgroundColor: "lightblue",
   },
 });
